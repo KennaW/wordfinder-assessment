14c9be9038acbcf2992e7a70eb820911b424b721
kennaw <kennawarsinske@gmail.com>
2015-03-13 11:11:45 -0700

    ACTUALLY add wordfindertest skeleton

---
+++ tests/WordFinderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+  require_once "src/WordFinder.php";
+
+  class WordFinderTest extends PHPUNIT_Framework_TestCase
+
+  {
+    function test_makeTitleCase_oneWord()
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+    }
+  }
