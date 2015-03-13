d11ae003d063cfce13a903b61bef64c4a1983a8f
kennaw <kennawarsinske@gmail.com>
2015-03-13 11:45:11 -0700

    switch computers

--- tests/WordFinderTest.php
+++ tests/WordFinderTest.php
@@ -5,12 +5,21 @@
   class WordFinderTest extends PHPUNIT_Framework_TestCase
 
   {
-    function test_makeTitleCase_oneWord()
+    function test_wordfinder_oneletter()
     {
+      //test WordFinder from src/wordFinder.php
       //Arrange
+      $test_WordFinder = new WordFinder;
+      $word = "a";
+      $sentance = "a";
 
       //Act
+      //result is the numeric output of checkWord from chekcword function
+      //word is the needle, sentance is the haystack
+      $result = $test_makeTitleCase->checkWord($word, $sentance);
 
       //Assert
+      $this->assertEquals(1, $result);
     }
   }
+?>
